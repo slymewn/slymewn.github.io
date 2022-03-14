d3b7a9bb933c88c6ef7b3b46db89fb53941025c5
slymewn <slymewn@gmail.com>
2022-03-14 10:29:57 +0700

    feat: add 3D

---
+++ precache-manifest.2d2869a004a78a6a8f4ced36fa1eadd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8796ca48c7c403774536",
+    "url": "/static/css/main.6bc63d58.chunk.css"
+  },
+  {
+    "revision": "8796ca48c7c403774536",
+    "url": "/static/js/main.f8e445cd.chunk.js"
+  },
+  {
+    "revision": "8911a827080028c65a8f",
+    "url": "/static/js/runtime~main.1d5bc325.js"
+  },
+  {
+    "revision": "36d73dada7ad1b7a7a2a",
+    "url": "/static/js/2.34a9fb21.chunk.js"
+  },
+  {
+    "revision": "17cafacefc0671cbaf3e",
+    "url": "/static/js/3.0a738eea.chunk.js"
+  },
+  {
+    "revision": "ef6dc19f77dc39b3888954dbfff7c89e",
+    "url": "/index.html"
+  }
+];
